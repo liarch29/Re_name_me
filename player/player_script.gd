cc3c901dedafcbea6093dd616e5cf10fd5b087a3
liarch29 <charliean19@gmail.com>
2017-11-12 15:19:45 -0800

    turn_test complete

--- player/player_script.gd
+++ player/player_script.gd
@@ -5,7 +5,7 @@ extends KinematicBody2D
 # var b = "textvar"
 const speed = 100
 var dir = 0.4
-var max_rot = 0.1
+var max_rot = 0.01
 var target = Vector2(get_pos())
 
 
@@ -16,23 +16,28 @@ func _ready():
 
 func _fixed_process(delta):
 	if get_rot() == dir:
-		if get_pos().distance_squared_to(target) > pow(speed * delta, 2):
-			var default = Vector2(0, 1).rotated(dir)
-			var veloc = default * speed * delta
-			move(veloc)
-		else:
-			set_pos(target)
+		forward(delta)
 	else:
 		var c_rot = angle_conv(get_rot())
 		var t_rot = angle_conv(dir)
 
 		var diff = t_rot - c_rot
+		forward(delta)
 		if abs(diff) < max_rot:
 			set_rot(dir)
 		elif (diff > 0 && diff < PI) || (diff > -2*PI && diff < -PI):
 			set_rot(get_rot()+max_rot)
 		else:
 			set_rot(get_rot()-max_rot)
+
+func forward(delta):
+	if get_pos().distance_squared_to(target) > pow(speed * delta, 2):
+		var default = Vector2(0, 1).rotated(get_rot())
+		var veloc = default * speed * delta
+		move(veloc)
+		dir = target.angle_to_point(get_pos())
+	else:
+		set_pos(target)
 
 func angle_conv(angle):
 	if angle < 0:
@@ -42,4 +47,3 @@ func angle_conv(angle):
 
 func set_target(pos):
 	target = pos
-	dir = target.angle_to_point(get_pos())
