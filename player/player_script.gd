62910800e1926f4bdcd49f1133c51be2c23fc6ed
liarch29 <charliean19@gmail.com>
2017-11-12 15:39:40 -0800

    removed dir

--- player/player_script.gd
+++ player/player_script.gd
@@ -4,27 +4,28 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 const speed = 100
-export(float) var dir
+export(float) var initial_rot = 0
 var max_rot = 0.01
 var target = Vector2(get_pos())
 
 
 func _ready():
 	set_fixed_process(true)
-	set_rot(dir)
+	set_rot(initial_rot)
 	pass
 
 func _fixed_process(delta):
-	if get_rot() == dir:
+	var angle_to_target = target.angle_to_point(get_pos())
+	if get_rot() == angle_to_target:
 		move_forward(delta)
 	else:
 		var current_rot = angle_conv(get_rot())
-		var target_rot = angle_conv(dir)
+		var target_rot = angle_conv(angle_to_target)
 
 		var diff = target_rot - current_rot
 		move_forward(delta)
 		if abs(diff) < max_rot:
-			set_rot(dir)
+			set_rot(angle_to_target)
 		elif (diff > 0 && diff < PI) || (diff > -2 * PI && diff < -PI):
 			set_rot(get_rot() + max_rot)
 		else:
@@ -35,7 +36,6 @@ func move_forward(delta):
 		var default = Vector2(0, 1).rotated(get_rot())
 		var veloc = default * speed * delta
 		move(veloc)
-		dir = target.angle_to_point(get_pos())
 	else:
 		set_pos(target)
 
