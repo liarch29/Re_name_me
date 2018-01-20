bc2c2bf2a33c0e4e1284506852dc6dfc625d77de
Bryan Tan <techniux@gmail.com>
2018-01-20 15:51:55 -0800

    Refactor movement approx line algorithm

--- player/MoveTargetIndicator.gd
+++ player/MoveTargetIndicator.gd
@@ -3,16 +3,21 @@ extends Node2D
 export(NodePath) var player
 
 var _player
-var average_delta = 0.015
-var time_out = 0
-var max_tick_till_time_out = 10000
-var guide_list = []
+
+# Rate at which to approximate movement line, in seconds
+var line_draw_rate = 0.1
+
+# Maximum amount of time to draw movement approx line, in seconds
+var max_draw_time = 10.0
+
+# Cache of points in the movement approx line
+var move_approx_cache = []
 
 
 func _ready():
 	_player = get_node(player)
 	self.position = self._player.position
-	set_process(true)
+	self.set_process(true)
 
 func _process(delta):
 	if _player.position == _player.target:
@@ -20,55 +25,53 @@ func _process(delta):
 	else:
 		self.show()
 		var color = get_node("indicator_shape").get_color()
-		draw_guide_line(color)
-		update()
+		recalculate_move_approx_line(color)
+		# Note: "update" as in "schedule a draw call"
+		self.update()
 
 func _draw():
 	if _player.position != _player.target:
 		var color = get_node("indicator_shape").get_color()
 		# Note: these coordinates are relative to this node
 		# draw_line(_player.position - self.position, Vector2(0, 0), color)
-		draw_all(color)
+		draw_move_approx_line(color)
 
-func draw_all(color):
-	var move_point1 = Vector2(_player.position)
-	for point in guide_list:
-		draw_line(move_point1-self.position, point-self.position, color)
-		move_point1 = point
+func draw_move_approx_line(color):
+	var previous = Vector2(_player.position)
+	for next in move_approx_cache:
+		draw_line(previous - self.position, next - self.position, color)
+		previous = next
 
-func draw_guide_line(color):
-	# var max_rot = _player.max_rot
-	guide_list = []
-	average_delta = 0.1
-	var move_point1 = Vector2(_player.position)
-	var move_point2 = Vector2(_player.position)
+func recalculate_move_approx_line(color):
+	move_approx_cache.clear()
+	var previous_point = Vector2(_player.position)
+	var next_point = Vector2(_player.position)
 	var rot = _player.rotation
-	var ticks_to_target = move_point1.distance_to(_player.target) / (_player.speed * average_delta)
-	var max_rot = _player.max_rot * average_delta
-	while(1):
-		move_point2 = move_point1
-		var angle_to_target = _player.target.angle_to_point(move_point1)
+	var max_rot = _player.max_rot * line_draw_rate
+	var draw_time = 0
+	var drawing = true
+	while drawing:
+		var angle_to_target = _player.target.angle_to_point(previous_point)
 		var current_rot = _player.angle_conv(rot)
 		var target_rot = _player.angle_conv(angle_to_target)
-		var diff = target_rot - current_rot
-		if move_point1.distance_squared_to(_player.target) > pow(_player.speed * average_delta, 2):
+		var angle_diff = target_rot - current_rot
+		if previous_point.distance_squared_to(_player.target) > pow(_player.speed * line_draw_rate, 2):
+			#TODO: Refactor to dedicated utility/physics class
 			var default = Vector2(1, 0).rotated(rot)
-			var veloc = default * _player.speed * average_delta
-			move_point1 += veloc
+			var veloc = default * _player.speed * line_draw_rate
+			previous_point += veloc
 		else:
-			move_point1 = (_player.target)
-		if abs(diff) < max_rot:
-			rot = (angle_to_target)
-		elif (diff > 0 && diff < PI) || (diff > -2 * PI && diff < -PI):
-			rot = (rot + max_rot)
+			previous_point = _player.target
+		if abs(angle_diff) < max_rot:
+			rot = angle_to_target
+		elif (angle_diff > 0 && angle_diff < PI) || (angle_diff > -2 * PI && angle_diff < -PI):
+			rot = rot + max_rot
 		else:
-			rot = (rot - max_rot)
-
-		if move_point1 == move_point2 || time_out > max_tick_till_time_out:
-			time_out = 0
-			break
-
-		time_out += 1
-		guide_list.append(move_point2)
-		#draw_line(move_point2 - self.position, move_point1 - self.position, color)
-
+			rot = rot - max_rot
+		
+		if previous_point == next_point or draw_time > max_draw_time:
+			drawing = false
+		else:
+			draw_time += line_draw_rate
+			move_approx_cache.append(next_point)
+		next_point = previous_point
